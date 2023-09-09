c38435930ea103974fee140bc43a7386524862aa
SeungminChae <csm4903@naver.com>
2023-09-10 04:37:29 +0900

    Update update_blogPost.py

--- update_blogPost.py
+++ update_blogPost.py
@@ -24,7 +24,7 @@ preREADME = """
 
 <br>
 
-[![Carefreelife's GitHub stats](https://github-readme-stats.vercel.app/api?username=carefreelife98)](https://github.com/anuraghazra/github-readme-stats)
+[![Carefreelife's GitHub stats](https://github-readme-stats.vercel.app/api?username=carefreelife98&show_icons=true&theme=dracular)](https://github.com/anuraghazra/github-readme-stats)
 
 <br><br>
 
@@ -76,30 +76,9 @@ preREADME = """
 > **Registry** <br>
 > <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white">
 
+<br><br>
 
-
-
-
-
-
-
-
-
-
-<!--
-**carefreelife98/carefreelife98** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
-Here are some ideas to get you started:
-
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+<h1>Latest Post of My Blog!!</h1>
 
 """
 
